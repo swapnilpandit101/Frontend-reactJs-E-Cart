cc128804200767ad229d8a79c462bd7a8f10da6b
swapnilpandit101 <swapnilpandit101@gmail.com>
2025-02-06 10:23:59 +0530

    corrected spelling mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Cart from './components/Cart';
 import { items } from './Mock-Data/Data';
 
 const App = () => {
-  const [data, setData] = useState([...items]);  // Initializing state for data (products)
-  const [cart, setCart] = useState([]);  // Initializing state for cart
+  const [data, setData] = useState([...items]);  
+  const [cart, setCart] = useState([]);  
 
   return (
     <>
